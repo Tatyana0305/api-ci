398c5c7c2152b41bc35eadebf5b0441f6d078f88
Tatyana Bondarenko <tanya_kursavka@mail.ru>
2022-03-23 06:28:36 +0300

    added accounts.schema.json

---
+++ src/test/java/ru/netologi/rest/MobileBankApiTestV1.java
@@ -0,0 +1,28 @@
+package ru.netologi.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
+
+public class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        //Given-When-Then
+
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+                .body("", hasSize(3))
+                .body("[0].id", equalTo(1))
+                .body("[2].currency", equalTo("RUB"))
+                .body("[2].balance", greaterThan(0));
+
+
+    }
+}
